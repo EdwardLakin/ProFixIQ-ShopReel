a01b63442d6eeeae3d27a13c440cc13166503587
EdwardLakin <edwardlakin35@gmail.com>
2026-03-16 05:21:48 +0000

    updated connect

--- src/features/shopreel/integrations/facebook/connect.ts
+++ src/features/shopreel/integrations/facebook/connect.ts
@@ -52,6 +52,7 @@ function buildFacebookScopes(): string {
     "pages_show_list",
     "pages_manage_metadata",
     "pages_read_engagement",
+    "pages_manage_posts",
     "business_management",
   ].join(",");
 }
